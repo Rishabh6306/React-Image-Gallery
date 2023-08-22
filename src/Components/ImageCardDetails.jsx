b8e2f7d762e8b14b998609074b809e33da324f4f
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-08-22 16:38:25 +0530

    Details Page & Related Images Done

--- src/Components/ImageCardDetails.jsx
+++ src/Components/ImageCardDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import fetchDataDetails from '../utils/fetchDataDetails';
+import ImageCard from './ImageCards';
 
 function ImageCardDetails() {
   const [photo, setPhoto] = useState({});
@@ -12,22 +13,30 @@ function ImageCardDetails() {
 
 
   return (
-    <div>
-      <Link to="/" className="text-2xl text-red-500 hover:underline my-4">
+    <>
+    <div className='bg-blue-900 text-white py-10 px-2 lg:p-10'>
+      <Link to="/" className="text-2xl float-left my-4 text-red-500 hover:underline">
         Home
       </Link>
       {Object.keys(photo).length > 0 && (
         <div>
-          <h2>Photo Details</h2>
-          <div>
-            <img src={photo.url} alt={photo.title} />
-            <div>Title: {photo.title}</div>
-            <div>Description: {photo.description}</div>
-            {/* Render other photo details as needed */}
+          <h1 className='text-center my-6 mt-20 sm:my-14 text-3xl md:text-5xl'>Details Page</h1>
+          <div className='flex flex-col sm:flex-row mt-1 sm:mt-14 w-full md:w-5/6'>
+            <img src={photo.url} alt={photo.title} className="custom-box-shadow sm:h-60 xl:h-96 rounded-3xl mx-4 p-1" />
+            <div className='flex flex-col justify-center mt-10 ml-4 lg:ml-20'>
+            <h1 className='text-3xl lg:text-4xl xl:text-5xl tracking-wider leading-snug font-semibold '>{photo.description}</h1>
+            <p className='mt-5 text-2xl tracking-wider '>{photo.title}</p>
+            </div>
           </div>
         </div>
       )}
     </div>
+
+    <div className='my-20'>
+      <h1 className='my-14 text-center text-5xl'>Related Images</h1>
+    <ImageCard/>
+    </div>
+    </>
   );
 }
 
--- src/utils/fetchdata.jsx
+++ src/utils/fetchdata.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 async function fetchData(setData) {
     try {
         const response = await axios.get('https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=20');
-        setData(response.data);
+       setData(response.data);
     } catch (error) {
         console.error('Error fetching data:', error);
     }
